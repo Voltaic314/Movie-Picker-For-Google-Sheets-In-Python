341c5b3e220379e9e2de4860b12d6ee94a2073ea
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-05-06 23:18:18 -0400

    Update Movie-Picker.py

--- Movie-Picker.py
+++ Movie-Picker.py
@@ -15,11 +15,15 @@ service = build('sheets', 'v4', credentials=creds)
 # Call the Sheets API
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=config.config_stuff['SAMPLE_SPREADSHEET_ID'],
-                            range="Media!A1:F999").execute()
+                            range="Media!A1:F999").execute() # Please note that the "Media!" part of this denotes the name of the sheet itself on the overall spreadsheet, make sure yours matches otherwise the code will not work. 
+# also note above that the A1:F999 represents the range. The code treats every row as a single list, so best to organize your media row by row as a single item. Your range can be whatever you need. 
 
 values = result.get('values', []) #get values from spreadsheet
 
 str_match = [s for s in values if "*No*" in s] #creates a variable for all the lines of the spreadsheet that contain *No*
 
-print("Media Title, Genre, Watched by Nyx? & Watched by Logan? - Nyx's Rating - Logan's Rating") #This prints this line into text to make the output easier to understand
-print(random.choice(str_match)) #This prints a single random output from all the values that match the str_match criteria
+#Note that each item in the following line represents a column in the spreadsheet we used, feel free to modify this line however to fit your needs, this has nothing to do with the actual spreadsheet, mainly just printing a line to understand what you are reading). 
+print("Media Title, Genre, Watched by Person A? & Watched by Person B? - Person A's Rating - Person B's Rating") #This prints this line into text to make the output easier to understand
+
+#This prints a single random output from all the values that match the str_match criteria
+print(random.choice(str_match)) 
